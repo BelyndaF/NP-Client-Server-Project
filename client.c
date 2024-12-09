8faabfda34a22bd3c58d0aa6fbb995c2fcf41a13
Belinda Foley <114702907+BelyndaF@users.noreply.github.com>
2024-12-08 22:53:33 -0600

    Update client.c

--- client.c
+++ client.c
@@ -1,72 +1,161 @@
-#include <arpa/inet.h>
-#include <netinet/tcp.h> // Include for TCP_NODELAY
+#include <arpa/inet.h> // inet_addr()
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h> // Include for setsockopt
-#include <unistd.h>
-
+#include <strings.h> // bzero()
+#include <sys/socket.h>
+#include <stdbool.h>
+#include <unistd.h> // read(), write(), close()
+#define MAX 80
 #define PORT 8080
+#define SA struct sockaddr
+void func(int sockfd)
+{
+    char buff[MAX];
+    int n;
+    for (;;) {
+        bzero(buff, sizeof(buff));
+        printf("\nPlease, choose an option to continue:\n");
+        printf("1. Login\n");
+        printf("2. Create Account\n");
+        printf("3. Exit\n");
+        printf("Option: ");
+        n = 0;
+        while ((buff[n++] = getchar()) != '\n')
+            ;
 
-int main() {
-  int sock = 0;
-  struct sockaddr_in serv_addr;
-  char buffer[1024] = {0};
+        write(sockfd, buff, sizeof(buff));
+        bzero(buff, sizeof(buff));
+        read(sockfd, buff, sizeof(buff));
+        // printf("\n%s\n", buff);
+        if ((strncmp(buff, "exit", 4)) == 0) {
+            printf("Client Exit...\n");
+            break;
+        } else if (strncmp(buff, "login", 5) == 0) { // If server asks for login, prompt user for credentials
+            printf("Enter username: ");
+            bzero(buff, sizeof(buff)); // Clear the buffer
+            fgets(buff, sizeof(buff), stdin); // Read the username
+            write(sockfd, buff, sizeof(buff)); // Send username to server
 
-  // Create socket
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    printf("\n Socket creation error \n");
-    return -1;
-  }
+            bzero(buff, sizeof(buff)); // Clear the buffer
+            printf("Enter password: ");
+            fgets(buff, sizeof(buff), stdin); // Read the password
+            write(sockfd, buff, sizeof(buff)); // Send password to server
 
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(PORT);
+            bzero(buff, sizeof(buff)); // Clear the buffer
+            read(sockfd, buff, sizeof(buff)); // Read server's response to login attempt
+            printf("%s\n\n", buff); // Print the server's response (success or failure)
 
-  // Convert IP addresses from text to binary form
-  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-    printf("\nInvalid address/ Address not supported \n");
-    return -1;
-  }
+            if (strncmp(buff, "You have successfully logged in!", sizeof(buff)) == 0) {
+                printf("Team D's Library Main Menu:\n");
+                printf("----------------------------------------------------------\n");
+                for (;;) {
+                    printf("\nPlease choose an option:\n1. Review Account Information\n2. View Books\n3. Rent Book\n4. Return Book\n5. Exit\nOption: ");
+                    bzero(buff, sizeof(buff));
+                    n = 0;
+                    while ((buff[n++] = getchar()) != '\n')
+                        ;
 
-  // Disable Nagle's algorithm by setting TCP_NODELAY option
-  int flag = 1;
-  if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(int)) < 0) {
-    perror("setsockopt TCP_NODELAY failed");
-    return -1;
-  }
+                    write(sockfd, buff, sizeof(buff));
 
-  // Connect to server
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    printf("\nConnection Failed \n");
-    return -1;
-  }
+                    if (strncmp(buff, "5", 1) == 0) {
+                        printf("Client Exit...\n");
+                        break;
+                    } else if (strncmp(buff, "1", 1) == 0) {
+                        char buffer2[1020];
+                        bzero(buffer2, sizeof(buffer2));
+                        read(sockfd, buffer2, sizeof(buffer2));
+                        printf("\n%s\n", buffer2);
+                    } else if (strncmp(buff, "2", 1) == 0) {
+                        char buffer2[2048];
+                        bzero(buff, sizeof(buff));
+                        bzero(buffer2, sizeof(buffer2));
+                        read(sockfd, buffer2, sizeof(buffer2));
+                        printf("\n%s\n", buffer2);
+                    } else if (strncmp(buff, "3", 1) == 0) {
+                        bzero(buff, sizeof(buff)); // Clear the buffer
+                        printf("Enter the book ID you want to rent: ");
+                        fgets(buff, sizeof(buff), stdin); // Read the ID
+                        write(sockfd, buff, sizeof(buff)); 
 
-  printf("Connected to the server. Type messages below:\n");
-  fflush(stdout); // Flush the stdout buffer immediately after printing
+                        bzero(buff, sizeof(buff));
+                        read(sockfd, buff, sizeof(buff));
+                        printf("\n%s\n", buff);
+                    } else if (strncmp(buff, "4", 1) == 0) {
+                        bzero(buff, sizeof(buff)); // Clear the buffer
+                        printf("Enter the book ID you want to return: ");
+                        fgets(buff, sizeof(buff), stdin); // Read the ID
+                        write(sockfd, buff, sizeof(buff)); 
 
-  while (1) {
-    // Receive message from server
-    memset(buffer, 0, sizeof(buffer));
-    read(sock, buffer, sizeof(buffer)); // Receive prompt from server
-    printf("%s", buffer);               // Print the received prompt
-    fflush(
-        stdout); // Flush the stdout buffer to ensure prompt appears immediately
+                        bzero(buff, sizeof(buff));
+                        read(sockfd, buff, sizeof(buff));
+                        printf("\n%s\n", buff);
+                    }
+                }
+                
+                break;
+            }
 
-    // Get user input and remove newline if present
-    fgets(buffer, sizeof(buffer), stdin);
-    buffer[strcspn(buffer, "\n")] = '\0'; // Remove newline character
+        } else if (strncmp(buff, "create", 6) == 0) { // If server prompts for account creation
+            printf("Enter a new username: ");
+            bzero(buff, sizeof(buff)); // Clear the buffer
+            fgets(buff, sizeof(buff), stdin); // Read the new username
+            write(sockfd, buff, sizeof(buff)); // Send the username to the server
 
-    // Send input to server
-    write(sock, buffer, strlen(buffer));
+            bzero(buff, sizeof(buff)); // Clear the buffer
+            printf("Enter a new password: ");
+            fgets(buff, sizeof(buff), stdin); // Read the new password
+            write(sockfd, buff, sizeof(buff)); // Send the password to the server
 
-    // Exit condition check
-    if (strcmp(buffer, "99") == 0) { // Exit if the user types "3" or "5"
-      printf("Exiting...\n");
-      fflush(stdout); // Ensure the exit message is flushed
-      break;
+            // bzero(buff, sizeof(buff)); // Clear the buffer
+            // printf("Enter your ZIP code: ");
+            // fgets(buff, sizeof(buff), stdin); // Read the new zip
+            // write(sockfd, buff, sizeof(buff)); // Send the zip to the server
+
+            bzero(buff, sizeof(buff)); // Clear the buffer
+            read(sockfd, buff, sizeof(buff)); // Read server's response to account creation attempt
+            printf("%s\n", buff); // Print the server's response (success or failure)
+        } 
     }
-  }
+}
 
-  close(sock);
-  return 0;
+int main()
+{
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
+
+    // connect the client socket to server socket
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
+        != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else{
+        printf("You have successfully connected to the Library Server!\n");
+        printf("----------------------------------------------------------\n");
+        printf("Welcome to Team D's Library Database!\n");
+        printf("----------------------------------------------------------\n");
+    }
+
+    // function for chat
+    func(sockfd);
+
+    // close the socket
+    close(sockfd);
 }
